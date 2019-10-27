4ca485bacef16b6c5ded6351daa7c82fb171710d
yosiassapari <yosiassapari@gmail.com>
2019-10-27 16:09:05 -0700

    fixi logo, title, author, and desc of the siteMetadata in gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Starter - Dimension V2",
-    author: "Hunter Chang",
-    description: "A Gatsby.js V2 Starter based on Dimension by HTML5 UP"
+    title: "International Papua Scholars Network",
+    author: "Richard Mehuze",
+    description: "Membangun jejaring akademik dan professional terbesar di Papua/Papua Barat"
   },
   plugins: [
     'gatsby-plugin-react-helmet',
